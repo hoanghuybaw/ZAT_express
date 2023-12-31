a286eabfd47302f03804d8668618fa914c4b7e29
HOANGHUY\admin <hoanghuybaw@gmail.com>
2023-12-31 19:43:13 +0700

    updateUi

--- js/include.js
+++ js/include.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     $('#navBar').load('navBar.html');
     $('#slider').load('slider.html');
     $('#service').load('service.html');
+    $('#banner').load('banner.html');
+    $('#network').load('network.html');
+    $('#introduce').load('introduce.html');
     $('#search').load('search.html');
     $('#footer').load('footer.html');
 });
